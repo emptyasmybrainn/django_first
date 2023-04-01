c7ac02ca43f34b2d740946f707b1714aef032311
emptyasmybrainn <bidyutbikashboruah999@gmail.com>
2023-04-02 01:02:03 +0530

    django_first

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')  #, {'name': name}
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = "Our service is very regular"
+    return render(request, 'index.html', {'feature': feature1})  #, {'name': name}
 
     # return HttpResponse('<h1> Hey, Welcome </h1>')
     # name = 'John'
@@ -16,6 +21,7 @@ def index(request):
 
 
 def counter(request):
-    text = request.GET['text']
+    # text = request.GET['text']
+    text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
